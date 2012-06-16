63b69a2cebd661982658e71cdeb0efbcf3c8e9bf
beezee <brian.zeligson@gmail.com>
2012-06-15 23:03:08 -0400

    initial commit

---
+++ readme.txt
@@ -0,0 +1,38 @@
+=== WP HeadJS ===
+Contributors: beezeee
+Donate link: http://www.brianzeligson.com
+Tags: headjs, javascript, wp_enqueue_script
+Requires at least: 2.9.1
+Tested up to: 3.2.1
+Stable tag: tags/0.1
+
+Uses HeadJS to load your enqueued scripts asynchronously, in parallel, executing them in order.
+
+== Description ==
+
+This plugin uses the wp_print_scripts action hook, as opposed to output buffering and regex used by alternative implementations.
+
+The downside of this method is that only scripts loaded via wp_enqueue_script will be affected by the plugin, the upside is better
+performance by avoiding output buffering on every page load.
+
+The plugin will preserve any localizations added via wp_localize_script, and uses the first parameter passed to wp_enqueue_script
+as the label for the script in the head.js call. For example,
+
+    wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js');
+    
+would show up as
+
+    head.js({"jquery": "https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"});
+    
+This allows you to run callbacks when specific scripts are ready, such as
+
+    head.ready('jquery', function() {
+        //do something when jquery is loaded
+    });
+    
+For more on HeadJS usage, see http://headjs.com/
+
+
+== Installation ==
+
+The easiest way is via Plugins->Add New from the left sidebar of your WP Admin, just search for category sticky posts.
---
+++ wp_headjs.php
@@ -0,0 +1,48 @@
+<?php
+/**
+Plugin Name: WP HeadJS
+Plugin URI: http://github.com/beezee/wp_headjs
+Description: Load all enqueued scripts asynchronously in parallel, and execute in order using <a href="http://headjs.com">HeadJS</a>.
+Version: 0.1
+Author: Brian Zeligson
+Author URI: http://www.brianzeligson.com
+*/
+
+if (!class_exists('wpHeadJS'))
+{
+    class wpHeadJS
+    {
+        public function __construct()
+        {
+            $this->add_actions();
+        }
+        
+        public function add_actions()
+        {
+            add_action('wp_print_scripts', array($this, 'headjs_enqueue'));
+        }
+        
+        public function headjs_enqueue()
+        {
+            global $wp_scripts;
+            $in_queue = $wp_scripts->queue;     
+            if (is_admin() or is_feed() or defined('XMLRPC_REQUEST') or empty($in_queue)) return;  
+            $scripts = array();
+            foreach($in_queue as $script)
+            {
+                if (is_array($wp_scripts->registered[$script]->extra) and
+                    isset($wp_scripts->registered[$script]->extra['data']))
+                            echo '<script type="text/javascript">'.$wp_scripts->registered[$script]->extra['data'].'</script>';
+                $src = $wp_scripts->registered[$script]->src;
+                $src = ( (preg_match('/^(http|https)\:\/\//', $src)) ? '' : get_bloginfo('url') ) . $src;
+                $scripts[] = '{"' . $script . '":"' . $src . '"}';
+            }
+            echo "<script type=\"text/javascript\" src=\"".get_bloginfo('wpurl') . "/wp-content/plugins/".basename(dirname(__FILE__))."/head.min.js\"></script>\n";
+            echo "<script type=\"text/javascript\">head.js(\n". implode(",\n", $scripts). "\n);</script>\n";
+            $wp_scripts->queue = array();
+        }
+        
+    }
+    
+    $headjs = new wpHeadJS();
+}
